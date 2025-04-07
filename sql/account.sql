7b6529f6b14792878539276f6bb269aa521a35ba
Arya <aryasinghbjc@gmail.com>
2025-04-07 15:42:58 +0530

    Updates acc table

--- sql/account.sql
+++ sql/account.sql
@@ -1,4 +1,4 @@
 USE ROLE ACCOUNTADMIN;
-CREATE OR REPLACE TABLE TEST.PUBLIC.account_ds(id NUMBER, name VARCHAR, account VARCHAR);
+CREATE OR REPLACE TABLE TEST.PUBLIC.account_ds(id NUMBER, name VARCHAR, salary VARCHAR);
 
-INSERT INTO employees (id, name, account) VALUES (1, 'Alice', '123'), (2, 'Bob', '456'), (3, 'Cindy', '789');
+INSERT INTO TEST.PUBLIC.account_ds (id, name, salary) VALUES (1, 'Alice', '123'), (2, 'Bob', '456'), (3, 'Cindy', '789');
