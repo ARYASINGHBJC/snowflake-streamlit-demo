cb1e743eca81f105f440d68f27e4c271f50c7c2e
Arya <aryasinghbjc@gmail.com>
2025-04-07 15:39:44 +0530

    Updates account table

--- sql/account.sql
+++ sql/account.sql
@@ -1,4 +1,4 @@
 USE ROLE ACCOUNTADMIN;
 CREATE OR REPLACE TABLE TEST.PUBLIC.account_ds(id NUMBER, name VARCHAR, account VARCHAR);
 
-INSERT INTO employees (id, name, account_no) VALUES (1, 'Alice', '123'), (2, 'Bob', '456'), (3, 'Cindy', '789');
+INSERT INTO employees (id, name, account) VALUES (1, 'Alice', '123'), (2, 'Bob', '456'), (3, 'Cindy', '789');
